76981511338ed7c3f657fbd9ecbf7e85b87878ed
Kotassu <cachilu@proton.me>
2025-05-28 14:32:39 -0400

    Created menu_promedios

---
+++ menu_promedios.py
@@ -0,0 +1,20 @@
+#promedios
+while True:
+    opc = int(input("¿Que desea hacer?\n1. Agregar nota\n2. Calcular promedio\n3. Salir\n"))
+    
+    if opc == 1:
+        cant_notas = int(input("¿Cuantas notas desea agregar?"))
+        list_notas = []
+        for i in range(cant_notas):
+            i += 1
+            notas = int(input(f"Ingrese la nota N°{i}: "))
+            list_notas.append(notas)
+    elif opc == 2:
+        suma = 0
+        for nota in list_notas:
+            suma = nota + suma
+        promedio = suma / len(list_notas)
+        print(f"El promedio es: {promedio}")
+    elif opc == 3:
+        print("Saliendo del programa...")
+        break
